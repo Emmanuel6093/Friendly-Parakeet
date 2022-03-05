41dac8c7be4617466abce144d83715cac99378dc
Emmanuel6093 <emmanue@Emmanuels-MacBook-Pro.local>
2022-03-05 16:21:05 -0500

    confirm statements

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,34 @@
-// Assignment code here
+// Prompt 
+function generatePassword() {
+  let charLength = prompt("How many characters would you like your password to contain?"); 
+
+
+  if (Number(charLength)) {
+    charLength = (Number(charLength));
+    if (charLength >= 8 && charLength <= 128) {
+      
+      let gainLower = confirm("Click Ok to confirm to include lowercase characters");
+
+      let gainUpper = confirm ("Click Ok to include lowercase letters?")
+
+      let gainNum = confirm ("Click Ok to include numeric characters");
+
+      let gainSpec = confirm("Click Ok to include special characters"); 
+
+
+      if (
+        gainLower ||
+        gainUpper ||
+        gainNum ||
+        gainSpec
+      ) {
+        let passwordText ="";
+      }
+    }
+  
+
+
+
 
 
 // Get references to the #generate element
@@ -15,3 +45,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+}
